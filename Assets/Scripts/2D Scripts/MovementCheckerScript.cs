5692929b17afbd0d05fa259687d2968f2d5a4c92
Joey van der Kaaij <joey.vanderkaaij@gmail.com>
2024-03-01 14:23:01 +0100

    THis too

---
+++ Assets/Scripts/2D Scripts/MovementCheckerScript.cs
@@ -0,0 +1,84 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class MovementCheckerScript : MonoBehaviour
+{
+    private CheckPhase checkPhase;
+
+    private float timer;
+
+    [SerializeField, Tooltip("The cooldown in seconds before the GameObject checks again."), Min(0)]
+    private float coolDown = 4; 
+    [SerializeField, Tooltip("How long before the checking actually starts")]
+    private float headsupCount = 1;
+
+    [SerializeField, Tooltip("Set the boats")]
+    private GameObject[] boats = null;
+
+    private List<Vector3> boatOldPositions = new List<Vector3>();
+
+    private void Start()
+    {
+        if (boats[0] != null)
+        {
+            foreach (var boat in boats)
+            {
+                boatOldPositions.Add(Vector3.zero);
+            }
+        }
+        else
+        {
+            Debug.LogError("No boats to check!");
+        }
+    }
+
+    void Update()
+    {
+        if (boats[0] != null)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= coolDown && checkPhase == CheckPhase.sleep)
+            {
+                Debug.Log("ABOUT TO CHECK");
+                checkPhase = CheckPhase.awakening;
+                timer = 0;
+            }
+            else if (timer >= headsupCount && checkPhase == CheckPhase.awakening)
+            {
+                Debug.Log("CHECKING MOVEMENT");
+                checkPhase = CheckPhase.awake;
+                for (int i = 0; i < boats.Length; i++)
+                {
+                    boatOldPositions[i] = boats[i].transform.position;
+                }
+                timer = 0;
+            }
+            else if (timer >= coolDown && checkPhase == CheckPhase.awake)
+            {
+                Debug.Log("NOT CHECKING ANYMORE");
+                checkPhase = CheckPhase.sleep;
+                timer = 0;
+            }
+
+            if (checkPhase == CheckPhase.awake)
+            {
+                for (int i = 0; i < boats.Length; i++)
+                {
+                    if (boatOldPositions[i] != boats[i].transform.position)
+                    {
+                        Debug.Log("YOU MOVED DURING CHECK WTF BRO?!?!?!??!??!?");
+                    }
+                }
+            }
+        }
+    }
+}
+
+public enum CheckPhase
+{
+    sleep,
+    awakening,
+    awake
+}
