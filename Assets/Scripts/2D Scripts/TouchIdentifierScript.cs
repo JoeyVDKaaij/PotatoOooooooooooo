aa4f56844dbc3a80f3819360e693a254917317aa
Joey van der Kaaij <joey.vanderkaaij@gmail.com>
2024-03-04 13:45:26 +0100

    More stuff
    Finalize dodge the obstacle
    Started shooting minigame

---
+++ Assets/Scripts/2D Scripts/TouchIdentifierScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TouchIdentifierScript : MonoBehaviour
+{
+    Vector3 touchPosWorld;
+
+    void Update() {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.CompareTag("Target"))
+                {
+                    hit.collider.GetComponent<TargetScript>().TargetHit();
+                }
+            }
+        }
+    }
+}
